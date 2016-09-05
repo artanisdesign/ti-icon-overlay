3f37c02c1bc6b38344a9351242a80b97ccaf68b5
Gergely Cziva <greg@dogfi.sh>
2016-09-05 12:14:07 +0200

    change match

--- index.js
+++ index.js
@@ -1,57 +1,49 @@
 #!/usr/bin/env node
 
-var fs = require('fs'),
-    gm = require('gm'),
+var fs = require("fs"),
+    gm = require("gm"),
     glob = require("glob"),
-    w, h, caption = "",
-    dir = "../**/appicon!(-Small)*.png";
+    caption = "",
+    dir = "/**/+(appicon|DefaultIcon)*.png";
 
-console.log("start");
-var args = process.argv.slice(2);
+//console.log("start");
 
-if(process.argv[2]){
+if (process.argv[2]) {
     caption = process.argv[2];
 }
-if(process.argv[3]){
-    dir = process.argv[3] + "/**/appicon!(-Small)*.png";
-    //console.log(dir);
+if (process.argv[3]) {
+    dir = process.argv[3] + "/**/+(appicon|DefaultIcon)*.png";
 }
 
-glob(dir, {}, function (er, files) {
-    //console.log(files);
-    for (var i=0; i < files.length; i++) {
+glob(dir, {}, function(er, files) {
+    for (var i = 0; i < files.length; i++) {
         processImage(files[i], files[i]);
     };
 });
 
-function processImage(input, output){
-    // obtain the size of an image
-    //console.log(input);
+function processImage(input, output) {
     gm(input)
-    .size(function (err, size) {
-      if (!err){
-            //console.log(size.width +" "+ size.height );
-            w = size.width;
-            h = size.height;
+        .size(function(err, size) {
+            if (!err) {
+                var w = size.width,
+                    h = size.height;
 
-            gm(input)
-            .stroke("#c20000")
-            .fill('#c20000')
-            .drawRectangle(0, h-h/5-5, w, h, 7,7)
-            .font("Arial.ttf")
-            .fontSize(Math.round(w/6))
-            .stroke("#ffffff",1)
-            .fill('#ffffff')
-            .drawText(0, 10, caption, "South")
-            .write(output, function (err) {
-              if (!err) {
-                console.log('Done: '+input);
-                }else{
-                    console.log(err);
-                }
-            });
-    }
-    });
+                gm(input)
+                    .stroke("#c20000")
+                    .fill('#c20000')
+                    .drawRectangle(0, h - h / 5 - 5, w, h, 7, 7)
+                    .font("Arial.ttf")
+                    .fontSize(Math.round(w / 6))
+                    .stroke("#ffffff", 1)
+                    .fill('#ffffff')
+                    .drawText(0, 10, caption, "South")
+                    .write(output, function(err) {
+                        if (!err) {
+                            console.log('Done: ' + input);
+                        } else {
+                            console.log(err);
+                        }
+                    });
+            }
+        });
 }
-
-
